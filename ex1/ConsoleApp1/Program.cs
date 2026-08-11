04f74ef0ad52d5a7ff36bc14d5413870fa25ecb1
davimrc655-alt <davimrc655@gmail.com>
2026-08-11 16:14:39 -0300

    Otimizacao de acessibilidade, tags semanticas e performance

---
+++ ex1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System ; 
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world"); 
+        }
+    }
+}
